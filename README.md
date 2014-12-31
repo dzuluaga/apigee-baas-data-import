b326eec870b57c5c05d12ab7ec119b8d30edbf93
dzuluaga <dzuluaga@apigee.com>
2014-12-31 11:50:40 -0500

    first basic tool

--- README.md
+++ README.md
@@ -1,21 +1,37 @@
 apigee-baas-data-import
 =======================
 
-This Node.js tool is meant to ease import of data to Apigee BaaS
+This Node.js tool is meant to ease import of data into Apigee BaaS.
 
 #### How to install it?
+This command will install BaaS Data import and will be available from command line to be executed as a regular unix command.
 ```
-npm install apigee-baas-data-import -g
+sudo npm install apigee-baas-data-import -g
 ```
 
 #### How to import a file?
 
 ```
-abaasimport -f ./{file.json} -c {clientId} -s {s3cr3tFromBassHere} -c {collection}
+abaasimport -f ./test/data.json -C testscoresimport -c {clientId} -s {secretBaaS} -O {org} -A {applicationName} -l -b
 ```
 
-### Get help?
+### Get help
 
 ```
-abaasimport --help
+$ abaasimport --help
+
+  Usage: abaasimport [options]
+
+  Options:
+
+    -h, --help                             output usage information
+    -V, --version                          output the version number
+    -A, --appname [appName]                Apigee BaaS App Name in which data will be imported
+    -b, --buildcurl                        build curl entries
+    -c, --clientid [clientId]              Apigee BaaS ClientId
+    -C, --collection [collectionName]      Apigee BaaS Collection in which data will be imported
+    -f, --file [filePath]                  JSON Import array file
+    -l, --logging                          generate logging http transaction entries from usergrid client
+    -O, --organization [organizationName]  Apigee BaaS Organization in which data will be imported
+    -s, --secret [secret]                  Apigee BaaS Secret
 ```
--- bin/abaasimport.js
+++ bin/abaasimport.js
@@ -1,21 +1,69 @@
 #!/usr/bin/env node
 
-/**
- * Module dependencies.
- */
-
 var program = require('commander');
+var usergrid = require('usergrid');
+var helper = require('../lib/helper');
 
 program
   .version('0.0.1')
-  .option('-p, --peppers', 'Add peppers')
-  .option('-P, --pineapple', 'Add pineapple')
-  .option('-b, --bbq', 'Add bbq sauce')
-  .option('-c, --cheese [type]', 'Add the specified type of cheese [marble]', 'marble')
+  .option('-A, --appname [appName]', 'Apigee BaaS App Name in which data will be imported')
+  .option('-b, --buildcurl', 'build curl entries')
+  .option('-c, --clientid [clientId]', 'Apigee BaaS ClientId')
+  .option('-C, --collection [collectionName]', 'Apigee BaaS Collection in which data will be imported')
+  .option('-f, --file [filePath]', 'JSON Import array file', './test/data.json')
+  .option('-l, --logging', 'generate logging http transaction entries from usergrid client')
+  .option('-O, --organization [organizationName]', 'Apigee BaaS Organization in which data will be imported')
+  .option('-s, --secret [secret]', 'Apigee BaaS Secret')
   .parse(process.argv);
 
-console.log('you ordered a pizza with:');
-if (program.peppers) console.log('  - peppers');
-if (program.pineapple) console.log('  - pineapple');
-if (program.bbq) console.log('  - bbq');
-console.log('  - %s cheese', program.cheese);
+console.log('you executed a BaaS import with:');
+if (program.file) console.log(program.file + '  - file');
+if (program.clientid) console.log(program.clientid + '  - clientid');
+if (program.secret) console.log(program.secret +'  - secret');
+console.log('  - %s cheese', program.collection);
+
+var fs = require('fs');
+var obj = JSON.parse(fs.readFileSync(program.file, 'utf8'));
+
+var client = new usergrid.client({
+    orgName: program.organization,//'MY_ORG1',
+    appName: program.appname, //'INSIGHTSDEMO',
+    authType: usergrid.AUTH_CLIENT_ID,
+    clientId: program.clientid,
+    clientSecret: program.secret,
+    logging: program.logging, //optional - turn on logging, off by default
+    buildCurl: program.buildcurl //optional - turn on curl commands, off by default
+});
+
+// //options object needs to have the type (which is the collection type)
+// var options = {
+//     type: program.collection,
+//     qs:{/*ql:'order by index'*/ limit : 1}
+// }
+
+// client.createCollection(options, function (err, dogs) {
+//     if (err) {
+//         //error - could not make collection
+//     } else {
+
+//         //success - new Collection worked
+
+//         //we got the dogs, now display the Entities:
+//         //console.log(dogs)
+//         //console.log(dogs.hasPreviousPage())
+//         //console.log(dogs)
+//         while(dogs.hasNextEntity()) {
+//             //get a reference to the dog
+//             dog = dogs.getNextEntity();
+//             //console.log(dog)
+//             var name = dog.get('userId');
+//             //notice('dog is called ' + name);
+//             //console.log('dog is called ' + name);
+//         }
+
+//         //success - looped through dogs
+
+//     }
+// });
+
+helper.createEntities(program.collection, obj.entities, client);
